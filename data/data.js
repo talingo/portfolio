e8b03b9d9d352d16c81dd16234ed89f73e15f526
Lautaro Llusa <llusa.lautaro@gmail.com>
2024-11-04 01:14:48 -0300

    added 1 project

--- data/data.js
+++ data/data.js
@@ -2,14 +2,13 @@ export const localData = {
   projects: [
     {
       _id: '1',
-      name: 'Fulbitoy',
-      displayName: 'Fulbitoy',
-      description:
-        'Fulbitoy is an app for scheduling and managing football games.',
-      url: 'https://talingo.github.io/fulbitoy/',
-      repositoryUrl: 'https://github.com/talingo/fulbitoy',
-      technologies: ['HTML', 'CSS', 'JS', 'REACT', 'NESTJS', 'TYPESCRIPT'],
-      image: './assets/images/projects/project1-duo-yellow.png',
+      name: 'Nightsky',
+      displayName: 'Nightsky',
+      description: 'Small minimalistic drawing practice.',
+      url: 'https://talingo.github.io/nightsky-animation/',
+      repositoryUrl: 'https://github.com/talingo/nightsky-animation',
+      technologies: ['HTML', 'CSS', 'JS'],
+      image: 'https://i.imgur.com/qeKnZgG.gif',
     },
     {
       _id: '2',
