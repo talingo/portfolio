6d64de79ce3190419514f35b3315f9064218e8d8
Lautaro Llusa <llusa.lautaro@gmail.com>
2024-11-16 17:39:12 -0300

    added project

--- data/data.js
+++ data/data.js
@@ -8,18 +8,18 @@ export const localData = {
       url: 'https://talingo.github.io/nightsky-animation/',
       repositoryUrl: 'https://github.com/talingo/nightsky-animation',
       technologies: ['HTML', 'CSS', 'JS'],
-      image: 'https://i.imgur.com/qeKnZgG.gif',
+      image: 'https://s11.gifyu.com/images/SyyxK.gif',
     },
     {
       _id: '2',
-      name: 'Fulbitoy',
-      displayName: 'Fulbitoy',
-      description:
-        'Fulbitoy is an app for scheduling and managing football games.',
-      url: 'https://talingo.github.io/fulbitoy/',
-      repositoryUrl: 'https://github.com/talingo/fulbitoy',
-      technologies: ['HTML', 'CSS', 'JS', 'REACT', 'NESTJS', 'TYPESCRIPT'],
-      image: './assets/images/projects/project1-duo-yellow.png',
+      name: 'Programming Books',
+      displayName: 'Programming Books',
+      description: 'Small View Transitions Demo',
+      url: 'https://bookshelf-lautaro.netlify.app/',
+      repositoryUrl: 'https://github.com/talingo/bookshelf-astro',
+      technologies: ['HTML', 'CSS', 'JS', 'ASTRO', 'TAILWIND'],
+      image:
+        'https://raw.githubusercontent.com/talingo/bookshelf-astro/main/assets/Animation.gif',
     },
     {
       _id: '3',
